a90bca3562f27de12bf46751434500aafe8a6ece
Jeff Sharer <44546014+PZ1Sharer05@users.noreply.github.com>
2019-02-20 18:36:04 +0700

    Add files via upload

---
+++ RPS Tutorial.py
@@ -0,0 +1,10 @@
+print ("Rock Scissor Paper Game")
+print ("By Jeff")
+menuChoice = input("Option\nPlay\nAbout\nChoose: ")
+if menuChoice == "Play":
+    #Play code
+    print ("Hello world")
+
+elif menuChoice == "About":
+    print ("2019 RPS Game by Jeff Sharer aka CS Club")
+    print ("Inspired by his best friend to make this game.")
